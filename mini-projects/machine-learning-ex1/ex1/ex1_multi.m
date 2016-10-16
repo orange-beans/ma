638873907d575ff8d40c78cf041c6d708b3aa860
Zhang Cheng Cheng <cczhang@SIMTech.a-star.edu.sg>
2016-10-16 19:47:48 +0800

    Complete ML EX1

--- mini-projects/machine-learning-ex1/ex1/ex1_multi.m
+++ mini-projects/machine-learning-ex1/ex1/ex1_multi.m
@@ -3,11 +3,11 @@
 %
 %  Instructions
 %  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
 %
-%  You will need to complete the following functions in this 
+%  This file contains code that helps you get started on the
+%  linear regression exercise.
+%
+%  You will need to complete the following functions in this
 %  exericse:
 %
 %     warmUpExercise.m
@@ -60,13 +60,13 @@ X = [ones(m, 1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -83,17 +83,28 @@ fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
 alpha = 0.01;
+alpha1 = 0.03;
+alpha2 = 0.1;
+alpha3 = 0.3;
+%num_iters = 400;
 num_iters = 400;
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
-
+% [theta, J_history1] = gradientDescentMulti(X, y, theta, alpha1, num_iters);
+% [theta, J_history2] = gradientDescentMulti(X, y, theta, alpha2, num_iters);
+% [theta, J_history3] = gradientDescentMulti(X, y, theta, alpha3, num_iters);
 % Plot the convergence graph
 figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
+
+% hold on;
+% plot(1:numel(J_history1), J_history1, '-r', 'LineWidth', 2);
+% plot(1:numel(J_history2), J_history2, '-g', 'LineWidth', 2);
+% plot(1:numel(J_history3), J_history3, '-y', 'LineWidth', 2);
 
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
@@ -105,7 +116,12 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
-
+% features need to be normalized
+% Note that the added ones no need to be normalized
+% Still maintain as ones
+[1,mu]
+[1,sigma]
+price = ([1, 1650, 3] - [0, mu]) ./ [1, sigma] * theta;
 
 % ============================================================
 
@@ -120,12 +136,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
@@ -150,10 +166,9 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+price = [1, 1650, 3] * theta;
 
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using normal equations):\n $%f\n'], price);
-
